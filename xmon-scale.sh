278e6a755cb4c81562c5283c51f3f0ab7c8e74c0
ashic <ashic@live.com>
2017-06-21 10:56:46 +0100

    added ihs settings

--- xmon-scale.sh
+++ xmon-scale.sh
@@ -4,10 +4,19 @@ function xscale {
   
     case $1 in
         els)
-            xrandr --output HDMI-1 --auto --scale 1.7x1.7 --output eDP-1 --auto --scale 0.7x0.7 --left-of HDMI-1
+            xrandr --output HDMI-1 --auto --scale 1.5x1.5 --panning 3840x2160+3840+0 --right-of eDP-1
+            xrandr --output eDP-1 --auto --scale 0.7x0.7
+            feh --recursive --randomize --bg-scale ~/.config/variety/Downloaded/*
+            ;;
+        ihs)
+            xrandr --output DP-1 --auto --left-of eDP-1
+            feh --recursive --randomize --bg-scale /mnt/e/ashic/Pictures/backgrounds/best/*
             ;;
         lap)
             xrandr --output eDP-1 --auto --scale 1x1
+            ;;
+        home)
+            xrandr --output DP-1 --left-of eDP-1 --auto
             ;;
         *)
             echo $"Usage: $0 [profile]"
